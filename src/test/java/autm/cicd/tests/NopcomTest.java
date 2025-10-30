500206ec37cd9fa47fc2e5dd0d23760028e370c0
Amal <amal2004@gmail.com>
2025-10-29 23:01:56 -0400

    updated chrome driver path

---
+++ src/test/java/autm/cicd/tests/NopcomTest.java
@@ -0,0 +1,48 @@
+package autm.cicd.tests;
+
+import java.nio.file.Paths;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class NopcomTest {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+    	//System.setProperty("webdriver.chrome.driver", "C:\\Users\\amalw\\eclipse-workspace\\autm.ci-cd\\src\\test\\resources\\chromedriver.exe");
+    	String driverPath = Paths.get("src", "test", "resources", "chromedriver.exe").toAbsolutePath().toString();
+    	System.setProperty("webdriver.chrome.driver", driverPath);
+    	
+    	ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));   
+    }
+    
+ 
+    @Test
+    public void verifyGoogleSearch() throws InterruptedException {
+        driver.get("http://localhost:5000/");
+        driver.findElement(By.id("small-searchterms")).sendKeys("Apple");
+        driver.findElement(By.xpath("//button[@class='button-1 search-box-button']")).click();
+        String title = driver.getTitle();
+        System.out.println(title);
+        Assert.assertTrue(title.contains("Search"), "Title mismatch!");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) driver.quit();
+    }
+}
